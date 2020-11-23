a254676a9b0e83942b876a40900009bbd7f80505
joeuhren <46763106+joeuhren@users.noreply.github.com>
2020-11-22 18:27:04 -0700

    Historical Address Balance

--- models/addresstx.js
+++ models/addresstx.js
@@ -5,7 +5,7 @@ var AddressTXSchema = new Schema({
   a_id: { type: String, index: true},
   blockindex: {type: Number, default: 0, index: true},
   txid: { type: String, lowercase: true, index: true},
-  amount: { type: Number, default: 0}
+  amount: { type: Number, default: 0, index: true}
 }, {id: false});
 
 module.exports = mongoose.model('AddressTx', AddressTXSchema);
