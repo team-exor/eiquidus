f8615794ff564a84227c942b30f34b1b07fcd0ba
joeuhren <46763106+joeuhren@users.noreply.github.com>
2020-11-22 17:00:44 -0700

    Remove the redundant need of get_stats after update_db

--- scripts/sync.js
+++ scripts/sync.js
@@ -140,10 +140,9 @@ is_locked(function (exists) {
               console.log('Run \'npm start\' to create database structures before running this script.');
               exit();
             } else {
-              db.update_db(settings.coin, function(){
-                db.get_stats(settings.coin, function(stats){
-                  if (settings.heavy == true) {
-                    db.update_heavy(settings.coin, stats.count, 20, function(){
+              db.update_db(settings.coin, function(stats){
+                if (settings.heavy == true) {
+                  db.update_heavy(settings.coin, stats.count, 20, function(){
 
                     });
                   }
@@ -168,7 +167,6 @@ is_locked(function (exists) {
                                   db.get_stats(settings.coin, function(nstats){
                                     console.log('reindex complete (block: %s)', nstats.last);
                                     exit();
-                                  });
                                 });
                               });
                             });
@@ -176,63 +174,63 @@ is_locked(function (exists) {
                         });
                       });
                     });
-                  } else if (mode == 'check') {
-                    db.update_tx_db(settings.coin, 1, stats.count, settings.check_timeout, function(){
-                      db.get_stats(settings.coin, function(nstats){
-                        console.log('check complete (block: %s)', nstats.last);
-                        exit();
-                      });
+                  });
+                } else if (mode == 'check') {
+                  db.update_tx_db(settings.coin, 1, stats.count, settings.check_timeout, function(){
+                    db.get_stats(settings.coin, function(nstats){
+                      console.log('check complete (block: %s)', nstats.last);
+                      exit();
                     });
-                  } else if (mode == 'update') {
-                    // Lookup the last block index
-                    Tx.findOne({}, {blockindex:1}).sort({blockindex:-1}).limit(1).exec(function(err, data){
-                      var nLast = stats.last;
-                      if (!err && data) {
-                        // start from the last block index
-                        nLast = data.blockindex;
-                      }
+                  });
+                } else if (mode == 'update') {
+                  // Lookup the last block index
+                  Tx.findOne({}, {blockindex:1}).sort({blockindex:-1}).limit(1).exec(function(err, data){
+                    var nLast = stats.last;
+                    if (!err && data) {
+                      // start from the last block index
+                      nLast = data.blockindex;
+                    }
 
-                      db.update_tx_db(settings.coin, nLast, stats.count, settings.update_timeout, function(){
-                        db.update_richlist('received', function(){
-                          db.update_richlist('balance', function(){
-                            db.get_stats(settings.coin, function(nstats){
-                              console.log('update complete (block: %s)', nstats.last);
-                              exit();
-                            });
+                    db.update_tx_db(settings.coin, nLast, stats.count, settings.update_timeout, function(){
+                      db.update_richlist('received', function(){
+                        db.update_richlist('balance', function(){
+                          db.get_stats(settings.coin, function(nstats){
+                            console.log('update complete (block: %s)', nstats.last);
+                            exit();
                           });
                         });
                       });
                     });
-                  } else if (mode == 'reindex-rich') {
-                    console.log('update started');
-                    db.update_tx_db(settings.coin, stats.last, stats.count, settings.check_timeout, function(){
-                      console.log('update finished');
-                      db.check_richlist(settings.coin, function(exists){
-                        if (exists == true) {
-                          console.log('richlist entry found, deleting now..');
-                      	}
-                        db.delete_richlist(settings.coin, function(deleted) {
-                          if (deleted == true) {
-                            console.log('richlist entry deleted');
-                          }
-                          db.create_richlist(settings.coin, function() {
-                            console.log('richlist created.');
-                            db.update_richlist('received', function(){
-                              console.log('richlist updated received.');
-                              db.update_richlist('balance', function(){
-                                console.log('richlist updated balance.');
-                                db.get_stats(settings.coin, function(nstats){
-                                  console.log('update complete (block: %s)', nstats.last);
-                                  exit();
-                                });
+                  });
+                } else if (mode == 'reindex-rich') {
+                  console.log('update started');
+                  db.update_tx_db(settings.coin, stats.last, stats.count, settings.check_timeout, function(){
+                    console.log('update finished');
+                    db.check_richlist(settings.coin, function(exists){
+                      if (exists == true) {
+                        console.log('richlist entry found, deleting now..');
+                      }
+                      db.delete_richlist(settings.coin, function(deleted) {
+                        if (deleted == true) {
+                          console.log('richlist entry deleted');
+                        }
+                        db.create_richlist(settings.coin, function() {
+                          console.log('richlist created.');
+                          db.update_richlist('received', function(){
+                            console.log('richlist updated received.');
+                            db.update_richlist('balance', function(){
+                              console.log('richlist updated balance.');
+                              db.get_stats(settings.coin, function(nstats){
+                                console.log('update complete (block: %s)', nstats.last);
+                                exit();
                               });
                             });
                           });
                         });
-                      }); 
-                    });
-                  }
-                });
+                      });
+                    }); 
+                  });
+                }
               });
             }
           });
