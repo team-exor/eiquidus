a538be2dbb48ed247d7ec984763453eb952ef32f
joeuhren <46763106+joeuhren@users.noreply.github.com>
2021-01-23 16:51:46 -0700

    Better loading of nested settings
    -Improved loading of settings including warning of missing and/or invalid settings and the ability to load the explorer using defaults without a settings.json file

--- scripts/sass_theme_reader.sh
+++ scripts/sass_theme_reader.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "\$theme-name: \"$(./node_modules/.bin/strip-json-comments settings.json | ./node_modules/.bin/json shared_pages.theme)\";" > ./public/css/_theme-selector.scss
+if [ -f ./settings.json ]; then
+  echo "\$theme-name: \"$(./node_modules/.bin/strip-json-comments settings.json | ./node_modules/.bin/json shared_pages.theme)\";" > ./public/css/_theme-selector.scss
+else
+  echo "\$theme-name: \"Exor\";" > ./public/css/_theme-selector.scss
+fi
