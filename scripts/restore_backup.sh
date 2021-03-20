d3aeb8d3cd987061a432d1ef298f1fff219ba395
joeuhren <joeuhren@protonmail.com>
2021-03-20 14:49:15 -0600

    Improved restore backup script
    -Greatly increases speed of database restore by removing the 'remove' cmds which are not only unnecessary but can be extremely time-consuming to run on large database collections while drop statements run very fast and free up everything required

--- scripts/restore_backup.sh
+++ scripts/restore_backup.sh
@@ -32,23 +32,14 @@ if [ -n "${1}" ]; then
       # Erase entire database
 sudo mongo <<EOF
 use explorerdb
-db.addresses.remove({})
 db.addresses.drop()
-db.addresstxes.remove({})
 db.addresstxes.drop()
-db.coinstats.remove({})
 db.coinstats.drop()
-db.heavies.remove({})
 db.heavies.drop()
-db.markets.remove({})
 db.markets.drop()
-db.masternodes.remove({})
 db.masternodes.drop()
-db.peers.remove({})
 db.peers.drop()
-db.richlists.remove({})
 db.richlists.drop()
-db.txes.remove({})
 db.txes.drop()
 exit
 EOF
